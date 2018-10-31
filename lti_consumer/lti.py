028076fc7a2eb7549049d7a20d96ccb361b49346
Felipe Montoya <felipe.montoya@edunext.co>
2018-10-31 17:40:54 -0500

    Adding the base changes to expose the field. Consent message is missing

--- lti_consumer/lti.py
+++ lti_consumer/lti.py
@@ -148,6 +148,7 @@ class LtiConsumer(object):
 
         self.xblock.user_email = ""
         self.xblock.user_username = ""
+        self.xblock.user_fullname = ""
         self.xblock.user_language = ""
 
         # Username, email, and language can't be sent in studio mode, because the user object is not defined.
@@ -157,6 +158,7 @@ class LtiConsumer(object):
             real_user_object = self.xblock.runtime.get_real_user(self.xblock.runtime.anonymous_student_id)
             self.xblock.user_email = getattr(real_user_object, "email", "")
             self.xblock.user_username = getattr(real_user_object, "username", "")
+            self.xblock.user_fullname = getattr(getattr(real_user_object, "profile", {}), "name", "")
             user_preferences = getattr(real_user_object, "preferences", None)
 
             if user_preferences is not None:
@@ -168,6 +170,8 @@ class LtiConsumer(object):
             lti_parameters["lis_person_sourcedid"] = self.xblock.user_username
         if self.xblock.ask_to_send_email and self.xblock.user_email:
             lti_parameters["lis_person_contact_email_primary"] = self.xblock.user_email
+        if self.xblock.ask_to_send_fullname and self.xblock.user_fullname:
+            lti_parameters["lis_person_name_full"] = self.xblock.user_fullname
         if self.xblock.user_language:
             lti_parameters["launch_presentation_locale"] = self.xblock.user_language
 
